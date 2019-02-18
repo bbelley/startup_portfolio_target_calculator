4255566c45fda7af2e1628b0db2b18f733044a27
CrowdWise <brianbelley@gmail.com>
2019-02-18 17:32:41 +1200

    Modified React app for IRR / exit multiple calculator to create portfolio calculator app for blog post. Requires input of assumed failure rate.

--- src/app.js
+++ src/app.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import numeral from 'numeral';
-import { Alert,Button,FormGroup,InputGroup,FormControl,Form,Col,ControlLabel,Radio } from 'react-bootstrap/lib';
+import { Alert,Button,FormGroup,InputGroup,FormControl,Form,Col,ControlLabel } from 'react-bootstrap/lib';
 
 
-class IRRApp extends React.Component {
+class PortfolioApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             invTime: '',
-            desiredMult: '',
             desiredIRR: '',
-            calcIRR: false,
-            calcMult: false,
-            chooseIRR: true,
-            chooseMult: false,
+            failureRate: '',
+            indivIRR: '',
+            indivReturn: '',
+            portReturn: false,
             error: ''
         };
         
         this.handleChange = this.handleChange.bind(this);
-        this.handleRadioChange = this.handleRadioChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleChange(event) {
@@ -31,122 +29,47 @@ class IRRApp extends React.Component {
             });
         };
     }
-    handleRadioChange(event) {
-        this.setState({
-            chooseIRR: !this.state.chooseIRR,
-            chooseMult: !this.state.chooseMult,
-            calcIRR: false,
-            calcMult: false,
-            desiredMult: '',
-            desiredIRR: ''
-        });
-    }
     handleSubmit(event) {
         event.preventDefault();
-        
-        if (this.state.chooseIRR) {
-            let yourMult = +this.state.desiredMult;
-            let yourTime = +this.state.invTime;
-
-            //Calculate IRR 
-            //Equation: IRR=(return_mult)^(1/time)-1
-            let yourIRR = Math.pow(yourMult,(1/yourTime))-1;
-            
+      
+        //Error Check
+        if (!this.state.desiredIRR || !this.state.invTime || !this.state.failureRate) {
+            //Return error state
             this.setState(() => {
                 return {
-                    calcIRR: yourIRR*100
+                    error: true,
+                    portReturn: false
                 };
             });
-
-            //Error Check
-            if (!this.state.desiredMult || !this.state.invTime) {
-                //Return error state
-                this.setState(() => {
-                    return {
-                        error: true
-                    };
-                });
-            } else {
-                this.setState(() => {
-                    return {
-                        error: ''
-                    };
-                });
-            };
         } else {
+            //Do Calculations for Portfolio
+            
             let yourIRR = +this.state.desiredIRR;
+            let yourFailure = +this.state.failureRate;
             let yourTime = +this.state.invTime;
 
-            //Calculate Exit Multiple 
-            //Equation: (IRR+1)^t=return_mult
-            let yourMult = Math.pow((yourIRR/100+1),yourTime);
+            let portMult = Math.pow((1+yourIRR/100),yourTime);
+            let indivMult = portMult/(1-yourFailure/100);
+            let indivRate = Math.pow(indivMult,(1/yourTime))-1;
             
             this.setState(() => {
                 return {
-                    calcMult: yourMult
+                    error: '',
+                    indivReturn: indivMult,
+                    indivIRR: indivRate*100,
+                    portReturn: portMult
                 };
             });
-
-            //Error Check
-            if (!this.state.desiredIRR || !this.state.invTime) {
-                //Return error state
-                this.setState(() => {
-                    return {
-                        error: true
-                    };
-                });
-            } else {
-                this.setState(() => {
-                    return {
-                        error: ''
-                    };
-                });
-            };
         };
-
         
     }   
     render() {
         return (
             <div align='center' style={{width: '100%'}} >
-            { this.state.error && <Alert bsStyle="danger" >Please enter both fields before calculating. </Alert> }
-            <Form style={{width: '70%', maxWidth:'300px'}} horizontal id="irr-calc" onSubmit={this.handleSubmit} autoComplete="off" >
-                <FormGroup>     
-                    <Radio
-                        name="radioGroup" 
-                        //inline 
-                        id="chooseIRR"
-                        defaultChecked={true}
-                        onChange={this.handleRadioChange}
-                    >         
-                        Calculate IRR
-                    </Radio>     
-                    
-                    <Radio 
-                        name="radioGroup" 
-                        //inline 
-                        id="chooseMult" 
-                        onChange={this.handleRadioChange}
-                    >         
-                        Calculate Exit/Return Multiple
-                    </Radio>     
-
-                </FormGroup>
+            { this.state.error && <Alert bsStyle="danger" >Please enter all three input fields before calculating. </Alert> }
+            <Form style={{width: '70%', maxWidth:'300px'}} horizontal id="portfolio-calc" onSubmit={this.handleSubmit} autoComplete="off" >
                 <FormGroup>
-                    <Col componentClass={ControlLabel} sm={7}>Exit/Return Multiple:</Col>
-                    <InputGroup> 
-                                <FormControl 
-                                    type="text" 
-                                    name="desiredMult" 
-                                    placeholder="e.g 3.60"
-                                    autoFocus 
-                                    value={this.state.desiredMult} 
-                                    onChange={this.handleChange}
-                                    disabled={this.state.chooseMult}
-                                /> 
-                                <InputGroup.Addon>X</InputGroup.Addon>
-                    </InputGroup>
-                    <Col componentClass={ControlLabel} sm={7}>IRR: </Col>
+                    <Col componentClass={ControlLabel} sm={7}>Target Portfolio IRR: </Col>
                     <InputGroup>
                             <FormControl 
                                 type="text" 
@@ -154,31 +77,47 @@ class IRRApp extends React.Component {
                                 placeholder="IRR" 
                                 value={this.state.desiredIRR} 
                                 onChange={this.handleChange}
-                                disabled={this.state.chooseIRR}
                             />
                             <InputGroup.Addon>%</InputGroup.Addon>
                     </InputGroup>
-                    <Col componentClass={ControlLabel} sm={7}>Time (years): </Col>
+                    <Col componentClass={ControlLabel} sm={7}>Time Horizon: </Col>
                     <InputGroup>
                             <FormControl 
                                 type="text" 
                                 name="invTime" 
-                                placeholder="Years" 
+                                placeholder="Time" 
                                 value={this.state.invTime} 
                                 onChange={this.handleChange}
                             />
                             <InputGroup.Addon>years</InputGroup.Addon>
                     </InputGroup>
+                    <Col componentClass={ControlLabel} sm={7}>Failure Rate: </Col>
+                    <InputGroup>
+                            <FormControl 
+                                type="text" 
+                                name="failureRate" 
+                                placeholder="0-100%" 
+                                value={this.state.failureRate} 
+                                onChange={this.handleChange}
+                            />
+                            <InputGroup.Addon>%</InputGroup.Addon>
+                    </InputGroup>
                     <br />
-                    <Button type="submit" bsStyle="primary">Calculate { (this.state.chooseMult) ? "Return Multiple" : "IRR" }</Button>
+                    <Button type="submit" bsStyle="primary">Calculate</Button>
                 </FormGroup>
             </Form>
             <br />
-                { (!this.state.error && this.state.calcIRR) ? (<Alert bsStyle="success" style={{width: '90%', maxWidth:'500px'}} >IRR =  <br /><strong> {numeral(this.state.calcIRR).format('0.00')}% </strong> </Alert> ) : 'Enter values above and click "Calculate"' }
-                { (!this.state.error && this.state.calcMult) ? (<Alert bsStyle="success" style={{width: '90%', maxWidth:'500px'}} >Exit/Return Multiple =  <br /><strong> {numeral(this.state.calcMult).format('0.00')} X </strong> </Alert> ) : '' }
+                { (!this.state.error && this.state.portReturn) ? (
+                    <div>
+                    <Alert bsStyle="success" style={{width: '90%', maxWidth:'500px'}} >Average Return Multiple for Non-Failed Investments =  <br /><strong> {numeral(this.state.indivReturn).format('0.00')} X <br />{numeral(this.state.indivIRR).format('0.00')}% IRR (each)</strong>  </Alert> 
+                    <Alert bsStyle="success" style={{width: '90%', maxWidth:'500px'}} >Overall Portfolio Return Multiple =  <br /><strong> {numeral(this.state.portReturn).format('0.00')} X </strong> </Alert>
+                    </div>
+                    ) 
+                    : 'Enter values above and click "Calculate"' 
+                }
             </div>
         );
     }
 }
 
-ReactDOM.render(<IRRApp />,document.getElementById('app'));
+ReactDOM.render(<PortfolioApp />,document.getElementById('app'));
